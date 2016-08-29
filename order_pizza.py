f4aa62b250a488fd1e1bbf8f9f682fef0b138217
Matt Smith <msmith@apaxsoftware.com>
2016-08-29 11:41:57 -0400

    Display current order.

--- order_pizza.py
+++ order_pizza.py
@@ -20,6 +20,18 @@ def display_invalid_option(menu_selection):
 def is_valid_pizza(pizza_selection, pizzas):
     return pizza_selection.isdigit() and int(pizza_selection)-1 < len(pizzas)
 
+def display_pizzas(pizzas):
+    if len(pizzas) > 0:
+        for index, pizza in enumerate(pizzas):
+            print("{}: {}  cost: ${}".format(index+1, pizza["name"], pizza["cost"]))
+    else:
+        print("No pizzas found.")
+
+def display_total_cost(pizzas):
+    total_cost = sum([pizza["cost"] for pizza in pizzas])
+    print("===========")
+    print("TOTAL COST: ${}".format(total_cost))
+
 def add_to_order():
     """
     Prompts for adding pizza to the order
@@ -27,8 +39,7 @@ def add_to_order():
 
     while True:
         print("\n")
-        for index, pizza in enumerate(PIZZAS):
-            print("{}: {}".format(index+1, pizza["name"]))
+        display_pizzas(PIZZAS)
         print("0: Go back")
 
         pizza_selection = input("\nWhich pizza would you like to order? ")
@@ -59,16 +70,16 @@ def main():
             break
         elif menu_selection == "1":
             add_to_order()
-            pass
         elif menu_selection == "2":
             pass
         elif menu_selection == "3":
-            pass
+            display_pizzas(my_pizzas)
+            display_total_cost(my_pizzas)
+            print("\n\n")
         elif menu_selection == "4":
             pass
         else:
             display_invalid_option(menu_selection)
-
 
 
 if __name__ == '__main__':
