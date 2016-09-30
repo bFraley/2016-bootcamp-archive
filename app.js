f8f79184e899911dfe3a5a9a9c66d482973dadbe
Matt Smith <msmith@apaxsoftware.com>
2016-09-30 13:35:17 -0400

    Wire up controller with template.

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 angular.module('app', [])
     .controller('MainCtrl', function ($scope) {
-        $scope.hello = "Sup Angular!";
+        $scope.userInput = '';
+
+        function getWordCount(argument) {
+            return 0;
+        }
+
+        function getCharacterCount(argument) {
+            return $scope.userInput.length;
+        }
+
+        function getMostCommonWord(argument) {
+            return 'n/a';
+        }
+
+        function getMostCommonCharacter(argument) {
+            return 'n/a';
+        }
+
+        $scope.getWordCount = getWordCount;
+        $scope.getCharacterCount = getCharacterCount;
+        $scope.getMostCommonWord = getMostCommonWord;
+        $scope.getMostCommonCharacter = getMostCommonCharacter;
+
     });
