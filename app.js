ecf4c0024689df2a8ecde57b7188db524db03bf9
Matt Smith <msmith@apaxsoftware.com>
2016-09-30 14:38:36 -0400

    Most common word.

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+'use strict';
 angular.module('app', [])
     .controller('MainCtrl', function ($scope) {
         $scope.userInput = '';
 
+        function cleanInput(input) {
+            return input
+                .replace(/[?.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+                .replace(/\s{2,}/g," ");
+        }
+
         function getWordCount(argument) {
-            return $scope.userInput.length && $scope.userInput.split(' ').length;
+            const cleanedUserInput = cleanInput($scope.userInput);
+            return cleanedUserInput.length && cleanedUserInput.split(' ').length;
         }
 
         function getCharacterCount(argument) {
@@ -11,7 +19,20 @@ angular.module('app', [])
         }
 
         function getMostCommonWord(argument) {
-            return 'n/a';
+            const cleanedUserInput = cleanInput($scope.userInput);
+            if (cleanedUserInput.length === 0) {
+                return 'n/a';
+            }
+
+            const words = cleanedUserInput.split(' ');
+            const wordCount = R.countBy(R.toLower)(words);
+            const wordCountPairs = R.toPairs(wordCount);
+            const sortedWordCountPairs = R.pipe(R.sortBy(R.prop(1)), R.reverse)(wordCountPairs)
+            return sortedWordCountPairs.length > 1 ? 
+                (
+                    sortedWordCountPairs[0][1] === sortedWordCountPairs[1][1] ? 
+                        'multiple matches' : sortedWordCountPairs[0][0]
+                ): sortedWordCountPairs[0][0];
         }
 
         function getMostCommonCharacter(argument) {
