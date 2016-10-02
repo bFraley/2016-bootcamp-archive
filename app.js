149f44db302e1868397cac0025322e7b8d5cf4e5
Matt Smith <msmith@apaxsoftware.com>
2016-10-02 11:58:57 -0400

    Refactor to make the word and character counts easier to read.

--- app.js
+++ app.js
@@ -1,60 +1,71 @@
 'use strict';
 angular.module('app', [])
-    .controller('MainCtrl', function ($scope) {
-        $scope.userInput = '';
+    .controller('MainCtrl', mainController);
 
-        function cleanInput(input) {
-            return input
-                .replace(/[?.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
-                .replace(/\s{2,}/g," ");
+function cleanInput(input) {
+    return input
+        .replace(/[?.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+        .replace(/\s{2,}/g," ");
+}
+
+const countMap = R.countBy(R.toLower);
+
+const sortPairsDescending = R.pipe(
+    R.sortBy(R.prop(1)),
+    R.reverse
+);
+
+const countPairs = R.toPairs;
+
+function mainController($scope) {
+    $scope.userInput = '';
+
+    function getWordCount() {
+        const cleanedUserInput = cleanInput($scope.userInput);
+        return cleanedUserInput.length && cleanedUserInput.split(' ').length;
+    }
+
+    function getCharacterCount() {
+        return $scope.userInput.length;
+    }
+
+    function getMostCommonWord() {
+        const cleanedUserInput = cleanInput($scope.userInput);
+        if (cleanedUserInput.length === 0) {
+            return 'n/a';
         }
 
-        function getWordCount() {
-            const cleanedUserInput = cleanInput($scope.userInput);
-            return cleanedUserInput.length && cleanedUserInput.split(' ').length;
+        const words = cleanedUserInput.split(' ');
+        const wordCount = countMap(words);
+        const wordCountPairs = countPairs(wordCount);
+        const sortedWordCountPairs = sortPairsDescending(wordCountPairs)
+        return sortedWordCountPairs.length > 1 ? 
+            (
+                sortedWordCountPairs[0][1] === sortedWordCountPairs[1][1] ? 
+                    'multiple matches' : sortedWordCountPairs[0][0]
+            ): sortedWordCountPairs[0][0];
+    }
+
+    function getMostCommonCharacter() {
+        const cleanedUserInput = cleanInput($scope.userInput);
+        if (cleanedUserInput.length === 0) {
+            return 'n/a';
         }
 
-        function getCharacterCount() {
-            return $scope.userInput.length;
-        }
+        const characters = cleanedUserInput.replace(/\s+/g, '');
+        const characterCount = countMap(characters);
+        const characterCountPairs = countPairs(characterCount);
+        const sortedCharacterCountPairs = sortPairsDescending(characterCountPairs)
+        return sortedCharacterCountPairs.length > 1 ? 
+            (
+                sortedCharacterCountPairs[0][1] === sortedCharacterCountPairs[1][1] ? 
+                    'multiple matches' : sortedCharacterCountPairs[0][0]
+            ): sortedCharacterCountPairs[0][0];
+    }
 
-        function getMostCommonWord() {
-            const cleanedUserInput = cleanInput($scope.userInput);
-            if (cleanedUserInput.length === 0) {
-                return 'n/a';
-            }
+    $scope.getWordCount = getWordCount;
+    $scope.getCharacterCount = getCharacterCount;
+    $scope.getMostCommonWord = getMostCommonWord;
+    $scope.getMostCommonCharacter = getMostCommonCharacter;
+}
 
-            const words = cleanedUserInput.split(' ');
-            const wordCount = R.countBy(R.toLower)(words);
-            const wordCountPairs = R.toPairs(wordCount);
-            const sortedWordCountPairs = R.pipe(R.sortBy(R.prop(1)), R.reverse)(wordCountPairs)
-            return sortedWordCountPairs.length > 1 ? 
-                (
-                    sortedWordCountPairs[0][1] === sortedWordCountPairs[1][1] ? 
-                        'multiple matches' : sortedWordCountPairs[0][0]
-                ): sortedWordCountPairs[0][0];
-        }
-
-        function getMostCommonCharacter() {
-            const cleanedUserInput = cleanInput($scope.userInput);
-            if (cleanedUserInput.length === 0) {
-                return 'n/a';
-            }
-
-            const characters = cleanedUserInput.replace(' ', '');
-            const characterCount = R.countBy(R.toLower)(characters);
-            const characterCountPairs = R.toPairs(characterCount);
-            const sortedCharacterCountPairs = R.pipe(R.sortBy(R.prop(1)), R.reverse)(characterCountPairs)
-            return sortedCharacterCountPairs.length > 1 ? 
-                (
-                    sortedCharacterCountPairs[0][1] === sortedCharacterCountPairs[1][1] ? 
-                        'multiple matches' : sortedCharacterCountPairs[0][0]
-                ): sortedCharacterCountPairs[0][0];
-        }
-
-        $scope.getWordCount = getWordCount;
-        $scope.getCharacterCount = getCharacterCount;
-        $scope.getMostCommonWord = getMostCommonWord;
-        $scope.getMostCommonCharacter = getMostCommonCharacter;
-
-    });
