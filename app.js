0ca4442f12c5f0c994467fae191b7c2ab1a71985
bFraley <bFraley@users.noreply.github.com>
2016-10-04 20:49:08 -0400

    form input adds new contacts to list and then clears form fields

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
+/* app.js - Copyright 2016 by Gary Ditsch and Brett Fraley */
+
+// Define Angular module and our addressController.
 angular.module('app', [])
     .controller('AddressCtrl', addressController);
 
-
-
 function addressController($scope) {
 
-    $scope.addressBook = [
-        {
-            name:'gary',
-            title:'boss',
-            phone:'345423523',
-            email:'g@g.com'
-        }
-    ];
+    $scope.addressBook = [];
 
+    // Define input values, and reset function.
     $scope.contactForm = {
-        name = '',
-        title = '',
-        phone = '',
-        email = ''
+        name: '',
+        title: '',
+        phone: '',
+        email: ''
     };
 
-    // Add a new contact on new form input.
+    $scope.resetContactForm = function() {
+        $scope.contactForm.name = '';
+        $scope.contactForm.title = '';
+        $scope.contactForm.phone = '';
+        $scope.contactForm.email = '';
+    };
+
+    // Add a new contact to addressBook on new form input.
     $scope.addNewContact = function() {
 
         var person = {
@@ -29,22 +31,23 @@ function addressController($scope) {
             title:$scope.contactForm.title,
             phone:$scope.contactForm.phone,
             email:$scope.contactForm.email
-        }
+        };
 
         $scope.addressBook.push(person);
 
-        console.log($scope.addressBook.length);
+        // Reset the contact form.
+        $scope.resetContactForm();
+    };
 
-    }
-
+    // Return addressBook array.
     $scope.getAddressBook = function() {
         return $scope.addressBook;
-    }
+    };
 
     // We call this to display a specific contact.
     $scope.viewContact = function() {
+    
 
-
-    }
+    };
 
 } // end addressController
