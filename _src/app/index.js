e183092359424616303bdd6bf8910a82ff2ad672
Matt Smith <msmith@apaxsoftware.com>
2016-10-07 08:16:28 -0400

    Add user detail.

--- _src/app/index.js
+++ _src/app/index.js
@@ -4,18 +4,29 @@ function AppController($http) {
     const ctrl = this;
     ctrl.seed = null;
     ctrl.users = [];
+    ctrl.currentUser = null;
 
-    $http.get('https://randomuser.me/api/', { 
-        params: {
-            results: 25,
-            seed: ctrl.seed,
-        }
-    }).then(function (response) {
-        ctrl.users = response.data.results;
-    })
+    function updateUsers () {
+        $http.get('https://randomuser.me/api/', { 
+            params: {
+                results: 25,
+                seed: ctrl.seed,
+            }
+        }).then(function (response) {
+            ctrl.users = response.data.results;
+            ctrl.currentUser = null;
+        });
+    }
+
+    updateUsers();
 
     ctrl.update = function () {
         ctrl.seed = ctrl.newSeed;
+        updateUsers();
+    }
+
+    ctrl.setUserDetail = function (index) {
+        ctrl.currentUser = ctrl.users[index];
     }
 }
 
--- bundle.js
+++ bundle.js
@@ -68,18 +68,29 @@
 	    var ctrl = this;
 	    ctrl.seed = null;
 	    ctrl.users = [];
+	    ctrl.currentUser = null;
 	
-	    $http.get('https://randomuser.me/api/', {
-	        params: {
-	            results: 25,
-	            seed: ctrl.seed
-	        }
-	    }).then(function (response) {
-	        ctrl.users = response.data.results;
-	    });
+	    function updateUsers() {
+	        $http.get('https://randomuser.me/api/', {
+	            params: {
+	                results: 25,
+	                seed: ctrl.seed
+	            }
+	        }).then(function (response) {
+	            ctrl.users = response.data.results;
+	            ctrl.currentUser = null;
+	        });
+	    }
+	
+	    updateUsers();
 	
 	    ctrl.update = function () {
 	        ctrl.seed = ctrl.newSeed;
+	        updateUsers();
+	    };
+	
+	    ctrl.setUserDetail = function (index) {
+	        ctrl.currentUser = ctrl.users[index];
 	    };
 	}
 	
