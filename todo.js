5a6dd4ddc0bb34e82f0e041253d3fbbc1c3140e0
bFraley <bFraley@users.noreply.github.com>
2016-10-10 02:20:53 -0400

    Implements order by alpha or date, plus show / hide new and save buttons

--- todo.js
+++ todo.js
@@ -10,23 +10,26 @@ function DDController($scope) {
     $scope.user_input = '';
     $scope.new_listname = '';
     $scope.current_list = '';
+    $scope.sort_alpha = false;
+    $scope.sort_date = false;
+    $scope.sort_def = true;
 
     $scope.modes = {
-        seeInputField: true,
-        seeDoneButton: true,
-        seeDeleteButton:true,
-        seeDateField:true
+        seeInputField: false,
+        seeSaveTask: false,
+        seeNewTask:true,
     }
 
     $scope.events = {
-        doneClicked: function() {
-            console.log("Done Clicked");
+        newClicked: function() {
+            $scope.modes.seeInputField = true;
+            $scope.modes.seeNewTask = false;
         }
     }
 
     // Initial single list.
     $scope.list = [
-        { content:'Item 1', datestamp:'DATE' },
+        { content:'Item 1', datestamp:'DATE', checked:false },
         { content:'Item 2', datestamp:'DATE' },
         { content:'Item 3', datestamp:'DATE' }
     ];
@@ -36,10 +39,11 @@ function DDController($scope) {
         if ($scope.user_input.length > 0) {
             content = $scope.user_input;
             datestamp = Date().split('2016')[0];
-
             $scope.list.push({ content:content, datestamp:datestamp});
             $scope.reset_user_input($scope.user_input);
-            console.log("Task Saved!");
+            $scope.modes.seeInputField = false;
+            $scope.modes.seeSaveTask = false;
+            $scope.modes.seeNewTask = true;
         } 
     }
     // Remove the task at the given index of list.
@@ -52,16 +56,18 @@ function DDController($scope) {
         $scope.user_input = ''
     }
 
-// Create a new list in the current group.
-    $scope.newlist = function() {
-        if (!$scope.new_listname)
-            return false;
-
-        $scope.all_lists.push($scope.new_listname);
-        $scope.new_listname = '';
+    // Sort todo list alphabetically.
+    $scope.alphaSort = function() {
+        $scope.sort_alpha = true;
+        $scope.sort_date = false;
+        $scope.sort_def = false;
     }
+
+    // Sort todo list by datestamp
+    $scope.dateSort = function() {
+        $scope.sort_alpha = false;
+        $scope.sort_def = false;
+        $scope.sort_date = true;
+    }
+
 };
-
-
-
-
